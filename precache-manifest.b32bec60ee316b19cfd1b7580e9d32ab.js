acd528228b8457dc5570d2bf60207d6c8a544cce
Adam Sawicki <adam.sawicki@symphony-solutions.eu>
2019-12-03 22:06:56 +0100

    Updates

---
+++ precache-manifest.b32bec60ee316b19cfd1b7580e9d32ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd097120f2fbc3cadd2facd7f28d9d6",
+    "url": "/advertising-data-etlv/index.html"
+  },
+  {
+    "revision": "4c1fbddd54a233f9fb81",
+    "url": "/advertising-data-etlv/static/css/main.9c30a809.chunk.css"
+  },
+  {
+    "revision": "939f628972cb2987b4e6",
+    "url": "/advertising-data-etlv/static/js/2.152cc82a.chunk.js"
+  },
+  {
+    "revision": "4c1fbddd54a233f9fb81",
+    "url": "/advertising-data-etlv/static/js/main.94c808b2.chunk.js"
+  },
+  {
+    "revision": "9ebaf84000684806a72c",
+    "url": "/advertising-data-etlv/static/js/runtime-main.b8d4e33b.js"
+  }
+]);
